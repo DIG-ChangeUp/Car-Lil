97a036eca573b4ad2ba4e8e46ac7ddf02f12a50d
SUKE <yosuke.hioki@gmail.com>
2024-12-16 13:31:23 +0900

    fixed package.json for deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ function setupServer() {
   const app = express();
   app.use(cors());
   app.use(express.json());
-  app.use("/", express.static("./dist"));
+  app.use("/", express.static( "./backend/dist"));
 
   app.get("/api", usersController.index)
 
