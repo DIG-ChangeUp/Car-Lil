6573ec5e678e921d39c7dbe9a9612951c3fa20a2
SUKE <yosuke.hioki@gmail.com>
2024-12-16 14:29:14 +0900

    made tsconfig file and tsx files

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,9 @@
 // import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import AppRoutes from "./AppRoutes.jsx";
+import { createRoot } from 'react-dom/client';
+import AppRoutes from "./AppRoutes.tsx";
 import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")!).render(
     // <StrictMode>
       <BrowserRouter>
         <AppRoutes />
