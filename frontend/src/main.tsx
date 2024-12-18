40a3fbf7cbe25b15abaff017f10c091aa56da858
kagi <hiroshi_kagiyama_aa@mail.toyota.co.jp>
2024-12-19 06:19:06 +0900

    プロジェクト全体にNoto Sans JPを適用

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { UIProvider } from '@yamada-ui/react';
+import { extendTheme, UIProvider, UIStyle } from '@yamada-ui/react';
 import App from './App.tsx';
+
+const globalStyle: UIStyle = {
+  body: {
+    fontFamily: 'Noto Sans JP',
+  },
+};
+const customTheme = extendTheme({ styles: { globalStyle } })();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <UIProvider>
+    <UIProvider theme={customTheme}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
