3f28c01e29082aa3378dcdbffd5eed4efb83de0a
kagi <hiroshi_kagiyama_aa@mail.toyota.co.jp>
2024-12-19 11:28:17 +0900

    NotFoundページ追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import SignUp from './pages/SignUp.tsx';
 import Home from './pages/Home.tsx';
 import { AuthProvider } from './components/AuthContext.tsx';
 import NewLogin from './pages/NewLogin.tsx';
+import OwnerDateRegistration from './pages/OwnerDateRegistration.tsx';
+import NotFound from './pages/NotFound.tsx';
 
 export default function App() {
   return (
@@ -15,6 +17,8 @@ export default function App() {
         <Route path="/login" element={<NewLogin />} />
         <Route path="/ownerTop" element={<OwnerTop />} />
         <Route path="/ownerSelectMenu" element={<OwnerSelectMenu />} />
+        <Route path="/calendar" element={<OwnerDateRegistration />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </AuthProvider>
   );
