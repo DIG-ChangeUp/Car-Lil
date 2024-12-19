e81739fb530b5e83c2c29a88072788a69674bccd
SUKE <yosuke.hioki@gmail.com>
2024-12-19 15:57:04 +0900

    made new OwnerSelectCar page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import SignUp from './pages/SignUp.tsx';
 import Home from './pages/Home.tsx';
 import { AuthProvider } from './components/AuthContext.tsx';
 import NewLogin from './pages/NewLogin.tsx';
+import OwnerSelectCar from './pages/OwnerSelectCar.tsx';
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<NewLogin />} />
         <Route path="/ownerTop" element={<OwnerTop />} />
+        <Route path="/ownerSelectCar" element={<OwnerSelectCar />} />
         <Route path="/ownerSelectMenu" element={<OwnerSelectMenu />} />
       </Routes>
     </AuthProvider>
