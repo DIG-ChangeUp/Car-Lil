feb6ce93a1421042ea7d6424dfd4fa68d212f259
kagi <hiroshi_kagiyama_aa@mail.toyota.co.jp>
2024-12-19 11:28:39 +0900

    貸し出し時間管理画面作成

---
+++ frontend/src/pages/OwnerDateRegistration.tsx
@@ -0,0 +1,7 @@
+import { Container } from '@yamada-ui/react';
+
+const OwnerDateRegistration = () => {
+  return <Container h="calc(100vh - 180px)"></Container>;
+};
+
+export default OwnerDateRegistration;
