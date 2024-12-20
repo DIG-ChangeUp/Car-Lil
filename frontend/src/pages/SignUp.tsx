74747277c1a24b6870b90bdacb5afa601e1460fc
kagi <hiroshi_kagiyama_aa@mail.toyota.co.jp>
2024-12-20 11:31:04 +0900

    userCredentialをコメントアウト

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -19,12 +19,13 @@ const SignUp: React.FC = () => {
     console.log('email:', email, '  /password:', password);
 
     try {
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      console.log('User created:', userCredential.user);
+      await createUserWithEmailAndPassword(auth, email, password);
+      // const userCredential = await createUserWithEmailAndPassword(
+      //   auth,
+      //   email,
+      //   password
+      // );
+      // console.log('User created:', userCredential.user);
       navigate('/');
     } catch (error: any) {
       console.error('Error creating user:', error);
